96833d39739c35e766349a739e585032269e9069
Brenn <djb2426@yahoo.com>
2026-05-09 12:08:23 +0100

    Updated project

--- workplaceSafetyService.js
+++ workplaceSafetyService.js
@@ -7,5 +7,20 @@ class WorkplaceSafetyService{
 		this.workers[];
 
 	}
+	addWorker(id, hoursWorked, taskCompleted, breakTaken){
+		const worker = {
+
+			id: id, 
+			hoursWorked: hoursWorked,
+			taskCompleted: taskCompleted,
+			breaksTaken: breaksTaken,
+			fatigueLevel: this.calculateFatigue(hoursWorked, breaksTaken)};
+			this.workers.push(worker);
+	
+			return worker;
+
+		}
+
+	}
 
 }
